31905d3b83573ad1ebd0c4c1f331fedd80543560
Alan Betanov <a.betanov@kassir.ru>
2025-03-11 21:34:37 +0300

    switch to hash mode routing for GitHub Pages compatibility

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import ProductsTable from '../components/ProductsTable.vue'
 import ProductDetails from '../components/ProductDetails.vue'
 import { useProductsStore } from '../stores/products'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
